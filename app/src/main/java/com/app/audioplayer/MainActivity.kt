e618d5b9901f6ed8acce805f79842c5c54c43f74
BillDai <billdai.dev@gmail.com>
2020-08-06 12:29:15 +0800

    feat: Show system pick file dialog to pick audio file and get its uri

--- app/src/main/java/com/app/audioplayer/MainActivity.kt
+++ app/src/main/java/com/app/audioplayer/MainActivity.kt
@@ -1,19 +1,62 @@
 package com.app.audioplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.observe
 import com.app.audioplayer.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
-    private val viewModel: ViewModel by viewModels<MainViewModel>()
+    private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        initViews()
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.release()
+        super.onDestroy()
+    }
+
+    private fun initViews() {
+        viewModel.audioUri.observe(this) {
+            binding.btnMainPickSong.isVisible = false
+            mediaPlayer = MediaPlayer.create(this@MainActivity, it).apply {
+                //start()
+            }
+        }
+
+        binding.btnMainPickSong.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "audio/mpeg"
+            }
+            startActivityForResult(intent, REQ_PICK_AUDIO_FILE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQ_PICK_AUDIO_FILE) {
+            val uri = data?.data ?: return
+            viewModel.audioUri.value = uri
+        }
+    }
+
+    companion object {
+        private const val REQ_PICK_AUDIO_FILE = 0
     }
 }
--- app/src/main/java/com/app/audioplayer/MainViewModel.kt
+++ app/src/main/java/com/app/audioplayer/MainViewModel.kt
@@ -1,7 +1,13 @@
 package com.app.audioplayer
 
+import android.net.Uri
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MainViewModel:ViewModel() {
+class MainViewModel(private val state: SavedStateHandle) : ViewModel() {
+    val audioUri = state.getLiveData<Uri>(STATE_AUDIO_URI)
 
+    companion object {
+        private const val STATE_AUDIO_URI = "audioUri"
+    }
 }
